d796e215555000b103a21a0b699e57f250e63da2
vmk <vmkz@cock.li>
2025-11-16 19:53:43 +0100

    update sheets

--- assets/portraits.js
+++ assets/portraits.js
@@ -1,4 +1,4 @@
-fetch("{{ site.baseurl }}/portraits.json")
+fetch("../portraits.json")
 	.then(r => r.json())
 	.then(data => {
 		const container = document.getElementById("portraits");
@@ -9,8 +9,8 @@ fetch("{{ site.baseurl }}/portraits.json")
 			const name = p.name;
 			const years = p.years;
 
-			const imgPath = `{{ site.baseurl }}/assets/portraits/${name}.jpg`;
-			const sheetPath = `{{ site.baseurl }}/sheet/${name}/index.html`;
+			const imgPath = `portraits/${name}.jpg`;
+			const sheetPath = `../sheet/${name}/index.html`;
 
 			const card = document.createElement("a");
 			card.href = sheetPath;
